05903e710e43c5075d0275d55066676b5d9a092b
Jerry Bai <jerrybai1@hotmail.com>
2018-02-28 16:20:41 +0800

    created recieveing program

--- recieve/recieve.ino
+++ recieve/recieve.ino
@@ -1,11 +1,34 @@
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
 
+//Pins
+const int pin_null= 10;
+const int pin_serial= 8;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //LCD pins
 
-void setup() {
-  // put your setup code here, to run once:
+//Definitions
+SoftwareSerial serial_pin(pin_serial, pin_null); //Rx, Tx (Tx not used)
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //LCD interface pins
 
+//Setup
+void setup() 
+{
+  Serial.begin(4800);
+  serial_pin.begin(9600);
+  lcd.begin(16, 2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+//Main loop
+void loop() 
+{
+  if (serial_pin.available())
+  {
+    String input_string = serial_URL.read();
+    Serial.write(input_string);
+    lcd.print(input_string);
+  }
 }
+
+
+
+
