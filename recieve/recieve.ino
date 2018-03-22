5cc9539c69907b66baf97e63f5d7f88532e5a0de
Jerry Bai <jerrybai1@hotmail.com>
2018-03-22 17:42:04 +0800

    Fixed printout of LCD text

--- recieve/recieve.ino
+++ recieve/recieve.ino
@@ -1,14 +1,20 @@
 #include <SoftwareSerial.h>
-#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h>
+#include <utility/Adafruit_MCP23017.h>
 
 //Pins
 const int pin_null= 10;
 const int pin_serial= 8;
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //LCD pins
+const int pin_LCD_1 = A1; //auto init
+const int pin_LCD_2 = A2; //auto init 
+
+//vars
+bool no_recieve_flag = true;
 
 //Definitions
 SoftwareSerial serial_pin(pin_serial, pin_null); //Rx, Tx (Tx not used)
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //LCD interface pins
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 //Setup
 void setup() 
@@ -16,16 +22,51 @@ void setup()
   Serial.begin(4800);
   serial_pin.begin(9600);
   lcd.begin(16, 2);
+  lcd.setBacklight(0x7);
+  lcd.print("Wait for pin");
 }
 
 //Main loop
 void loop() 
 {
-  while (serial_pin.available())
+  //first instance of signal recieved
+  if (serial_pin.available())
   {
-    char input = serial_pin.read();
-    Serial.write(input);
-    lcd.print(input);
+    lcd.clear();
+    lcd.print("Recieved: ");
+    //loop to print each char in the pipeline
+    while (serial_pin.available())
+    {
+      char input = serial_pin.read();
+      lcd.print(input);
+      Serial.write(input);
+      if (input == '.')
+      {
+        break;
+      }
+    }
+    //display the recieved pin for half a second
+    delay(1000);
+    //clear screen
+    lcd.clear();
+  }
+  else
+  {
+    lcd.clear();
+    lcd.print("Wait for pin");
+    delay(500);
+  }
+}
+
+void scrollprint(String text, int txtlength)
+{
+  lcd.setCursor(0, 0);
+  lcd.print(text);
+   for (int positionCounter = 0; positionCounter < txtlength; positionCounter++) {
+    // scroll one position right:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(150);
   }
 }
 
--- transmit/transmit.ino
+++ transmit/transmit.ino
@@ -2,8 +2,8 @@
 
 //Pins
 int pin_null  = 5;
-int pin_serial = 6;
-int pin_button = 10;
+int pin_serial = 7;
+int pin_button = 2;
 
 
 //Definitions
@@ -11,28 +11,30 @@ String code = "1234.";
 SoftwareSerial serial_pin(pin_null, pin_serial); //Rx, Tx. (RX not used)
 
 //Setup
-void setup() 
+void setup()
 {
-  Serial.begin(4800);
+  Serial.begin(9600);
   serial_pin.begin(9600);
-  pinMode(pin_button, INPUT);
+  pinMode(pin_button, INPUT_PULLUP);
 }
 
 //Main loop
 void loop() 
 {
   //if button is pressed
-  if (digitalRead(pin_button))
+  if (!digitalRead(pin_button))
   { 
     Serial.print("Button Pressed");
+    Serial.print(!digitalRead(pin_button));
     char buf[10];
     code.toCharArray(buf, 10);
 
-    //while button is down, code is sent every 500ms
-    while (digitalRead(pin_button))
+    //while button is down, code is sent every second
+    while (!digitalRead(pin_button))
     {
       serial_pin.write(buf);
-      delay(500);
+      Serial.print(buf);
+      delay(1000);
     }
   }
 }
